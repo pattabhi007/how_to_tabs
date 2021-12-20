2049fa5728e2e2f571795698b918113408cb9b1e
Pattabhi Reddy <pattabhi007@gmail.com>
2021-12-21 01:19:30 +0800

    Installed Jake

---
+++ Jakefile.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2021 Reddy Brothers LLC. All rights reserved. For license, see "README" or "LICENSE" file.
+(function () {
+    "use strict";
+
+    desc("Default build");
+    task("default", function () {
+        console.log("\n\nBUILD OK");
+    });
+
+}());
--- build.js
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2021 Reddy Brothers Corp. All rights reserved. For license, see "README" or "LICENSE" file.
-(function (){
-    "use strict";
-
-    console.log("Hello world");
-}());
